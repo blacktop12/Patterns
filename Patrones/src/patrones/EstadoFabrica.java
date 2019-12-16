ceec1a50fa497154af10d7fc767a8be26f2bfdfe
blacktop12 <57530914+blacktop12@users.noreply.github.com>
2019-12-16 09:38:36 -0500

    Update

--- Patrones/src/patrones/EstadoFabrica.java
+++ Patrones/src/patrones/EstadoFabrica.java
@@ -4,6 +4,5 @@ public abstract class EstadoFabrica
 {
 	protected Estado objState;
 
-	    // Método que deberán crear las clases que hereden de ésta
 	public abstract void mostrar();
 }
--- Patrones/src/patrones/LineaCerrada.java
+++ Patrones/src/patrones/LineaCerrada.java
@@ -6,8 +6,6 @@ public class LineaCerrada extends EstadoFabrica
         this.objState = objState;
     }
 
-    // -------------------------------------------
-
     @Override
     public void mostrar() {
         System.out.println("Alerta roja");
